5ec445f335ecceb26101434f9489896ded19e70d
Trần Đức Long <82155754+Longtd1605@users.noreply.github.com>
2025-08-28 15:40:40 +0700

    Update team_bot.py

--- team_bot.py
+++ team_bot.py
@@ -28,7 +28,7 @@ groups = [
 def login():
     import tempfile
     options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
